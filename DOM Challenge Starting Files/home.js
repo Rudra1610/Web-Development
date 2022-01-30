19088335c25dbd9dade146ec364241437b0b57d4
Rudra Sakariya <rudrasakariya16@gmail.com>
2022-01-30 19:36:14 +0530

    Dice challenge 1

--- DOM Challenge Starting Files/home.js
+++ DOM Challenge Starting Files/home.js
@@ -1,7 +1,1 @@
 document.querySelector("ul").lastElementChild.innerHTML = "Hello";
-document.getElementsByTagName("li")[0].innerHTML = "abc";
-
-console.log("hahaha");
-let a = prompt("hahaha");
-console.log(a);
-document.getElementsByTagName("h1").firstElementChild.innerHTML = a;
