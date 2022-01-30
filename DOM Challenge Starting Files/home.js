9c52361caa0651a50675308330bdf8f6a0a2ad60
Rudra Sakariya <rudrasakariya16@gmail.com>
2022-01-30 17:24:48 +0530

    Done

---
+++ DOM Challenge Starting Files/home.js
@@ -0,0 +1,7 @@
+document.querySelector("ul").lastElementChild.innerHTML = "Hello";
+document.getElementsByTagName("li")[0].innerHTML = "abc";
+
+console.log("hahaha");
+let a = prompt("hahaha");
+console.log(a);
+document.getElementsByTagName("h1").firstElementChild.innerHTML = a;
